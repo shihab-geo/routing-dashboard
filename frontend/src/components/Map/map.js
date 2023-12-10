0fdb491bf46ee956ba6f603d7bc00aebf1d89812
shihab-geo <shihab.hossain@bkash.com>
2023-12-10 17:28:03 +0600

    Map is updated

--- frontend/src/components/Map/map.js
+++ frontend/src/components/Map/map.js
@@ -37,7 +37,9 @@ export const Map = forwardRef((props, ref) => {
     const formatTime = (totalSeconds) => {
         const hours = Math.floor(totalSeconds / 3600);
         const minutes = Math.floor((totalSeconds % 3600) / 60);
-        const seconds = totalSeconds % 60;
+        // const seconds = totalSeconds % 60;
+        const seconds = Math.round((totalSeconds % 60) * 100) / 100; // Round to two decimal places
+
 
         const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
         return formattedTime;
