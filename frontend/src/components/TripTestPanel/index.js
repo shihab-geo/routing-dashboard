1c820253d571d05b8152b5fdae68c53be935f736
shihab-geo <shihab.hossain@bkash.com>
2023-12-13 16:04:09 +0600

    TripTest comp is updated

--- frontend/src/components/TripTestPanel/index.js
+++ frontend/src/components/TripTestPanel/index.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import { AudioOutlined } from '@ant-design/icons';
+import React from 'react';
+import { Input, Space } from 'antd';
+const { Search } = Input;
+
+
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: '#1677ff',
+    }}
+  />
+);
+const onSearch = (value, _e, info) => console.log(info?.source, value);
+
 
 export const TripTestPanel = () => {
   return (
-    <div>index</div>
+    <Space direction="vertical">
+      <Search
+        placeholder="Input DSO Number"
+        onSearch={onSearch}
+        style={{
+          width: 'auto',
+        }}
+      />
+    </Space>
   )
 }
 
