850edfc00cf265c8092242824db5417ff2e6218a
shihab-geo <shihab.hossain@bkash.com>
2023-12-14 12:06:11 +0600

    popup info box is updated

--- frontend/src/components/PopUpInfoBox/index.js
+++ frontend/src/components/PopUpInfoBox/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { Card, Space } from 'antd';
 import "./index.css";
 import { useSelector, useDispatch } from "react-redux";
+import * as STRING from "../../strings";
+
 
 
 export const PopUpInfoBox = () => {
 
+    const getActivePanel = useSelector((state) => state.select.panelInfo.activePanel);
     const getDuration = useSelector((state) => state.select.routingInfo.duration);
-    const getDistance = useSelector((state) => state.select.routingInfo.distance)
+    const getDistance = useSelector((state) => state.select.routingInfo.distance);
 
     return (
         
