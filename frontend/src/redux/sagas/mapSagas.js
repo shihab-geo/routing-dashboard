5259520575b75bcc574a134765f8a6c019c4256f
shihab-geo <shihab.hossain@bkash.com>
2023-12-14 09:44:58 +0600

    map saga is updated

--- frontend/src/redux/sagas/mapSagas.js
+++ frontend/src/redux/sagas/mapSagas.js
@@ -10,6 +10,7 @@ import {
 } from '../../services/api.service';
 import {
     getAgentPoints, setAgentPoints,
+    getDistHouseLoc, setDistHouseLoc,
 } from "../slices/mapSlice";
 import {
     setDistributor,
@@ -20,9 +21,7 @@ function* trackAgentsWorker({ payload }) {
     try {
         const response = yield call(fetchData, payload.url, payload.query, payload.variables)
         const agentData = pointGeoJsonFromGeom(response.data.data.data)
-        // console.log(trackingData);
-        console.log(agentData);
-        console.log(agentData?.features[0]?.properties?.distributor);
+
         if (response.status === 200) {
             yield put(setAgentPoints({ data: agentData }));
             yield put(setDistributor({ data: agentData?.features[0]?.properties?.distributor }));
@@ -35,14 +34,34 @@ function* trackAgentsWorker({ payload }) {
 }
 
 
+function* trackDistHouseLocWorker({ payload }) {
+    try {
+        const response = yield call(fetchData, payload.url, payload.query, payload.variables)
+        const distHouseData = pointGeoJsonFromGeom(response.data.data.data)
+
+        if (response.status === 200) {
+            yield put(setDistHouseLoc({ data: distHouseData }));
+        } else {
+            // console.log(response.status);
+        }
+    } catch (error) {
+        // console.log(error);
+    }
+}
+
+
 
 function* getAgentLocPoints() {
-    console.log('here');
     yield takeLatest(getAgentPoints.type, trackAgentsWorker);
+}
+
+function* getDistributorLocPoints() {
+    yield takeLatest(getAgentPoints.type, trackDistHouseLocWorker);
 }
 
 
 export const RoutingBenchmarkerMapSagas = [
     fork(getAgentLocPoints),
+    fork(getDistributorLocPoints),
 
 ];
