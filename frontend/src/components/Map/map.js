2460051f19bad6e4639ef00b0ff2a014fa67c545
shihab-geo <shihab.hossain@bkash.com>
2023-12-14 15:50:05 +0600

    map is updated

--- frontend/src/components/Map/map.js
+++ frontend/src/components/Map/map.js
@@ -41,18 +41,6 @@ export const Map = forwardRef((props, ref) => {
 
     const { mapLayers, mapSources } = useSelector((state) => state.mapreducer);
 
-    // //Format the time
-    // const formatTime = (totalSeconds) => {
-    //     const hours = Math.floor(totalSeconds / 3600);
-    //     const minutes = Math.floor((totalSeconds % 3600) / 60);
-    //     // const seconds = totalSeconds % 60;
-    //     const seconds = Math.round((totalSeconds % 60) * 100) / 100; // Round to two decimal places
-
-
-    //     const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    //     return formattedTime;
-    // }
-
 
     //Calculate the Routing Distance
     const calculateRouteDistance = () => {
